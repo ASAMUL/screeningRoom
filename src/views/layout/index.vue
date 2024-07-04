d9f5104d3e8fc7911dd60cd5ec385842764f8653
leo <1171385717@qq.com>
2024-07-04 18:53:41 +0800

    css: 样式调整

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,10 +1,38 @@
 <template>
-  <div class="layout-container flex flex-col w-full h-full">
-    <div class="navbar flex h-14 items-center gap-9">
-      <div class="logo mr-auto flex">
-        <n-icon size="40" color="#0e7a0d" icon-color="black">
-          <game-controller />
+  <div class="layout-container flex flex-col w-full h-full bg-[#121a21]">
+    <div
+      class="navbar flex h-14 items-center gap-9 px-4 border-b border-gray-500"
+    >
+      <!-- Logo -->
+      <div class="logo mr-auto flex items-center">
+        <n-icon size="20" color="#fff">
+          <Bonfire />
         </n-icon>
+        <span class="text-xl font-bold text-white">VidSync</span>
+      </div>
+      <!-- 导航栏 -->
+      <div class="nav flex items-center gap-4">
+        <n-button type="text" size="small" class="text-white"> 主页 </n-button>
+        <n-button type="text" size="small" class="text-white"> 订阅</n-button>
+      </div>
+      <!-- 图标导航栏 -->
+      <div class="nav flex items-center gap-2 cursor-pointer h-3/4">
+        <div
+          class="nav-icon flex items-center gap-2 bg-[#253646] px-3 py-3 rounded-lg"
+        >
+          <n-icon size="20" color="#fff"> <VideocamOutline /> </n-icon>
+        </div>
+        <div
+          class="nav-icon flex items-center gap-2 bg-[#253646] px-3 py-3 rounded-lg"
+        >
+          <n-icon size="20" color="#fff"> <SearchOutline /> </n-icon>
+        </div>
+        <div
+          class="nav-icon flex items-center gap-2 bg-[#253646] px-3 py-3 rounded-lg"
+        >
+          <n-icon size="20" color="#fff"> <User /> </n-icon>
+        </div>
+        flex
       </div>
     </div>
     <RouterView />
@@ -13,7 +41,7 @@
 
 <script setup>
 import { ref } from "vue";
-import { GameControllerOutline, GameController } from "@vicons/ionicons5";
+import { Bonfire, VideocamOutline, SearchOutline } from "@vicons/ionicons5";
 import { RouterView } from "vue-router";
 </script>
 
